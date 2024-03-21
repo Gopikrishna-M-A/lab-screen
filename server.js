13c72f009bcec83d75106c5b3c41dce474231901
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2024-03-21 21:01:57 +0530

    origin updated

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:3000", // Origin 1
-      "https://lab-screen.onrender.com", // Origin 2
+      "http://localhost:3000", 
+      "https://lab-screen.onrender.com",
+      "https://screenshare.sset.co.in",
     ],
     methods: ["GET", "POST"],
   },
